54453f538931c9186ad4f2bc3bc9fe933060933e
imskanand <the.anand3099@gmail.com>
2023-04-01 20:58:06 +0530

    feat/Models: Created Model component to render the Register and Login Models

---
+++ app/components/model/Model.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import Button from "../Button";
+
+interface ModelProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactNode;
+  footer?: React.ReactNode;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: () => string;
+}
+
+const Model: React.FC<ModelProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  footer,
+  actionLabel,
+  disabled,
+  secondaryAction,
+  secondaryActionLabel,
+}) => {
+  const [showModel, setShowModel] = useState(isOpen);
+
+  useEffect(() => {
+    setShowModel(isOpen);
+  }, [isOpen]);
+
+  const handleClose = useCallback(() => {
+    if (disabled) return;
+    setShowModel(false);
+    setTimeout(() => {
+      onClose();
+    }, 300);
+  }, [disabled, onClose]);
+
+  const handleSubit = useCallback(() => {
+    if (disabled) return;
+    onSubmit();
+  }, [disabled, onSubmit]);
+
+  const handleSecondaryAction = useCallback(() => {
+    if (disabled || !secondaryAction) return;
+    secondaryAction();
+  }, [disabled, secondaryAction]);
+
+  if (!isOpen) return null;
+
+  return (
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden bg-neutral-800/70 outline-none focus:outline-none">
+        <div className="relative mx-auto my-6 h-full w-full md:h-auto md:w-4/5 lg:h-auto lg:w-2/5 xl:w-2/5">
+          {/* Content */}
+          <div
+            className={`translate h-full duration-300 
+            ${showModel ? "translate-y-0" : "translate-y-full"} ${
+              showModel ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <div className="translate foucs:outline-none relative flex h-full w-full flex-col rounded-lg border-0 bg-white outline-none lg:h-auto">
+              {/* Header */}
+              <div className="relative flex items-center justify-center rounded-t border-b-[1px] p-6">
+                <button
+                  className="absolute left-9 border-0 p-1 transition hover:opacity-70 "
+                  onClick={handleClose}
+                >
+                  {" "}
+                  <IoMdClose size={18} />
+                </button>
+                <div className="text-lg font-semibold">{title}</div>
+              </div>
+              {/* Body  */}
+              <div className="relative flex-auto p-6">{body}</div>
+              {/* Footer */}
+              <div className="ga-2 flex flex-col p-6">
+                <div className="flex w-full flex-row items-center gap-4">
+                  {secondaryAction && secondaryActionLabel && (
+                    <Button
+                      outline
+                      disabled={disabled}
+                      label={secondaryActionLabel}
+                      onClick={handleSecondaryAction}
+                    />
+                  )}
+                  <Button
+                    disabled={disabled}
+                    label={actionLabel}
+                    onClick={handleSubit}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Model;
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata = {
 
 import { Nunito } from "next/font/google";
 import ClientOnly from "./components/ClientOnly";
+import Model from "./components/model/Model";
 const font = Nunito({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font.className`}>
         <ClientOnly>
+          <Model actionLabel="Submit" isOpen title="Login Please"/>
           <Navbar />
         </ClientOnly>
         {children}
